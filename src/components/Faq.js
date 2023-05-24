7ef27b5c09ff99493354686c33beec98fdb4f236
Yumie Tsuzuki <yumie.t.29@gmail.com>
2023-05-24 15:48:49 -0400

    add style

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -23,19 +23,29 @@ export default function Faq() {
 
   return (
     <>
+      <h1 className="text-2xl font-bold m-2">Frequently asked questions</h1>
       {qas.map((qa, i) => (
-        <div key={i}>
-          <h1>
-            <button type="button" onClick={() => handleClick(i)}>
+        <div
+          key={i}
+          className="border border-neutral-500 rounded-md m-2 mr-20 p-5"
+        >
+          <h2 className="text-lg font-semibold flex justify-start content-center">
+            <button
+              type="button"
+              className="mr-2"
+              onClick={() => handleClick(i)}
+            >
               {isOpen[`question${[i + 1]}`] ? (
-                <HiChevronDown />
+                <HiChevronDown className="w-7 h-7 text-emerald-600" />
               ) : (
-                <HiChevronRight />
+                <HiChevronRight className="w-7 h-7 text-emerald-600" />
               )}
             </button>
             {qa.question}
-          </h1>
-          {isOpen[`question${[i + 1]}`] && <p>{qa.answer}</p>}
+          </h2>
+          {isOpen[`question${[i + 1]}`] && (
+            <p className="text-base ml-8 text-emerald-400">{qa.answer}</p>
+          )}
         </div>
       ))}
     </>
